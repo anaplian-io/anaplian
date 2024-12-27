770087018c74b9f12f6f73db4d703018d97e330d
Ethan Fortner <ethfort@amazon.com>
2024-12-27 00:03:22 -0600

    feat: initial commit
    Initial release for @anaplian/core

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    files: ['**/*.{ts}'],
+    ignores: ['coverage/', 'dist/', 'node_modules/'],
+  },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
