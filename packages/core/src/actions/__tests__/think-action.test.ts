799ec4be8aebe1945eb90c482eec3ee0b1f498c1
Ethan Fortner <ethfort@amazon.com>
2025-01-05 14:20:33 -0600

    feat(core): implement the "think" action

---
+++ packages/core/src/actions/__tests__/think-action.test.ts
@@ -0,0 +1,16 @@
+import { Action } from '../action';
+import { ThinkAction } from '../think-action';
+
+describe('ThinkAction', () => {
+  it('echoes the input to the output', async () => {
+    const action: Action = new ThinkAction();
+    await expect(
+      action.apply({
+        thought:
+          'I’m very important. I have many leather-bound books and my apartment smells of rich mahogany.',
+      }),
+    ).resolves.toBe(
+      'I’m very important. I have many leather-bound books and my apartment smells of rich mahogany.',
+    );
+  });
+});
