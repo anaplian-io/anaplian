261f621c62ea7854a0ecf3cfb04b38089e266afe
Ethan Fortner <ethfort@amazon.com>
2025-01-05 14:20:33 -0600

    feat(core): implement the "think" action

---
+++ packages/core/src/actions/think-action.ts
@@ -0,0 +1,40 @@
+import { Action, ActionArgument } from './action';
+
+/**
+ * This action echoes the input to the output. This is meant for the agent to consider
+ * its actions before taking them and introspect about results.
+ */
+export class ThinkAction implements Action {
+  public readonly name: string = 'think';
+  public readonly arguments?: ActionArgument[] = [
+    {
+      name: 'thought',
+      description: 'The thought that will be saved to history.',
+    },
+  ];
+  public readonly description: string =
+    'This action echoes its input to the output. This will be retained in history, ' +
+    'but it will not be published externally. This is useful for considering and reasoning prior to taking other actions ' +
+    'or introspecting on results after taking another action.';
+  public readonly examples?: {
+    readonly arguments?: string[];
+    readonly result: string;
+  }[] = [
+    {
+      arguments: ['I should plan my next few actions before taking them.'],
+      result: 'I should plan my next few actions before taking them.',
+    },
+    {
+      arguments: [
+        'That did not turn out how I expected. I will change strategies when I try again.',
+      ],
+      result:
+        'That did not turn out how I expected. I will change strategies when I try again.',
+    },
+  ];
+  public readonly apply = async (
+    args: Record<string, string>,
+  ): Promise<string> => {
+    return args['thought']!;
+  };
+}
