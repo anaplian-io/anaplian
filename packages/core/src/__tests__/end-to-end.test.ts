c233df3a486c025dadb8dbd89410e73bfdefdab4
Ethan Fortner <ethfort@amazon.com>
2025-01-06 21:42:56 -0600

    fix(core): fatalError event is now executed prior to shut down

--- packages/core/src/__tests__/end-to-end.test.ts
+++ packages/core/src/__tests__/end-to-end.test.ts
@@ -90,4 +90,61 @@ describe('@anaplian/core', () => {
     );
     expect(invokeSpy).toHaveBeenCalledTimes(1);
   });
+
+  it('throws a fatal error', async () => {
+    const model = new FakeLLM({});
+    const onFatal = jest.fn().mockReturnValue(Promise.resolve());
+    const onStartIteration = jest.fn().mockReturnValue(Promise.resolve());
+    const onEndIteration = jest.fn().mockReturnValue(Promise.resolve());
+    const onShutdown = jest.fn().mockReturnValue(Promise.resolve());
+    const onInitialize = jest.fn().mockReturnValue(Promise.resolve());
+    const invokeSpy = jest
+      .spyOn(model, 'invoke')
+      .mockRejectedValue(new Error());
+    const agent = await new AgentBuilder({
+      modelName: 'gpt-4o-mini',
+      model: model,
+      roleAssignmentDirective: 'You are an agent. You do agent things.',
+    })
+      .addAction(new NopAction())
+      .addAction(new ThinkAction())
+      .addContextProvider(new DateContextProvider(), 0.5)
+      .addContextProvider(new HistoryContextProvider({}), 99.5)
+      .setOn('afterInitialize', onInitialize)
+      .setOn('beforeIterationStart', onStartIteration)
+      .setOn('afterIterationEnd', onEndIteration)
+      .setOn('beforeShutdown', onShutdown)
+      .setOn('fatalError', onFatal)
+      .build();
+    const runPromise = agent.run();
+    await yieldToEventLoop();
+    await runPromise;
+    expect(onFatal).toHaveBeenCalledTimes(1);
+    expect(onStartIteration).toHaveBeenCalledTimes(1);
+    expect(onStartIteration).toHaveBeenCalledWith(
+      expect.objectContaining({
+        history: {
+          records: [],
+        },
+      }),
+    );
+    expect(onEndIteration).toHaveBeenCalledTimes(0);
+    expect(onShutdown).toHaveBeenCalledTimes(1);
+    expect(onShutdown).toHaveBeenCalledWith(
+      expect.objectContaining({
+        history: {
+          records: [],
+        },
+      }),
+    );
+    expect(onInitialize).toHaveBeenCalledTimes(1);
+    expect(onInitialize).toHaveBeenCalledWith(
+      expect.objectContaining({
+        history: {
+          records: [],
+        },
+      }),
+    );
+    expect(invokeSpy).toHaveBeenCalledTimes(1);
+  });
 });
