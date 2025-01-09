8a112b948882e89557725bcf863cc33cde7a30fd
Ethan Fortner <ethfort@amazon.com>
2025-01-08 19:08:58 -0600

    fix(web): export the HTTP GET action

---
+++ packages/web/src/actions/index.ts
@@ -0,0 +1,7 @@
+export {
+  TavilySearchAction,
+  TavilySearchResult,
+  TavilySearchActionProps,
+} from './tavily-search-action';
+export { isTavilySearchResult } from './tavily-search-action.guard';
+export { HttpGetAction } from './http-get-action';
