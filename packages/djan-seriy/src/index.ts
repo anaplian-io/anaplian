e595e2e6277b2de51511737e94ccf4425a8da334
Ethan Fortner <ethfort@amazon.com>
2025-01-13 21:29:26 -0600

    feat(djan-seriy): added discord support to the agent

--- packages/djan-seriy/src/index.ts
+++ packages/djan-seriy/src/index.ts
@@ -11,61 +11,90 @@ import { ChatOpenAI } from '@langchain/openai';
 import { getEnvironmentVariable } from './environment';
 import { TavilySearchAction, HttpGetAction } from '@anaplian/web';
 import { tavily } from '@tavily/core';
+import { Client, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+import {
+  GuildContextProvider,
+  ListChannelsAction,
+  MessageChannelAction,
+} from '@anaplian/discord';
 
 const openAiApiKey = getEnvironmentVariable('OPEN_AI_API_KEY');
 const directive = getEnvironmentVariable('DIRECTIVE');
 const tavilyApiKey = getEnvironmentVariable('TAVILY_API_KEY');
-const modelName = 'gpt-4o-mini';
+const discordToken = getEnvironmentVariable('DISCORD_BOT_TOKEN');
 let djanSeriy: AnaplianAgent | undefined;
-new AgentBuilder({
-  modelName,
-  roleAssignmentDirective: directive,
-  model: new ChatOpenAI({
-    apiKey: openAiApiKey,
-    model: modelName,
-    streaming: false,
-    temperature: 0,
-  }),
-})
-  .addAction(new NopAction())
-  .addAction(new ThinkAction())
-  .addAction(
-    new TavilySearchAction({
-      tavilyClient: tavily({ apiKey: tavilyApiKey }),
-      maxResults: 5,
-    }),
-  )
-  .addAction(new HttpGetAction())
-  .addContextProvider(new HistoryContextProvider({}), 99.5)
-  .addContextProvider(new DateContextProvider(), 0.5)
-  .setOn('beforeShutdown', async (context) => {
-    console.info('SHUTTING DOWN...');
-    console.info(JSON.stringify(context));
-  })
-  .setOn('afterIterationEnd', async (context) => {
-    const history = context.history;
-    if (isHistoryContext(history)) {
-      const lastItem = history.records[history.records.length - 1];
-      if (lastItem) {
-        console.info(`AGENT: ${lastItem.actionTaken}`);
-        console.info(`RESULT: ${lastItem.result}`);
-        if (lastItem.actionTaken === 'nop()') {
-          await djanSeriy?.shutdown();
+const discordClient = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+discordClient
+  .login(discordToken)
+  .then(() => {
+    console.info('CONNECTED TO DISCORD');
+    const modelName = 'gpt-4o-mini';
+    return new AgentBuilder({
+      modelName,
+      roleAssignmentDirective: directive,
+      model: new ChatOpenAI({
+        apiKey: openAiApiKey,
+        model: modelName,
+        streaming: false,
+        temperature: 0,
+      }),
+    })
+      .addAction(new NopAction())
+      .addAction(new ThinkAction())
+      .addAction(
+        new TavilySearchAction({
+          tavilyClient: tavily({ apiKey: tavilyApiKey }),
+          maxResults: 5,
+        }),
+      )
+      .addAction(
+        new ListChannelsAction({
+          discordClient,
+        }),
+      )
+      .addAction(
+        new MessageChannelAction({
+          discordClient,
+        }),
+      )
+      .addAction(new HttpGetAction())
+      .addContextProvider(new HistoryContextProvider({}), 97.5)
+      .addContextProvider(new DateContextProvider(), 0.5)
+      .addContextProvider(
+        new GuildContextProvider({
+          discordClient,
+        }),
+        2,
+      )
+      .setOn('beforeShutdown', async (context) => {
+        console.info('SHUTTING DOWN...');
+        console.info(JSON.stringify(context));
+      })
+      .setOn('afterIterationEnd', async (context) => {
+        const history = context.history;
+        if (isHistoryContext(history)) {
+          const lastItem = history.records[history.records.length - 1];
+          if (lastItem) {
+            console.info(`AGENT: ${lastItem.actionTaken}`);
+            console.info(`RESULT: ${lastItem.result}`);
+            if (lastItem.actionTaken === 'nop()') {
+              await djanSeriy?.shutdown().then(() => process.exit(0));
+            }
+          }
         }
-      }
-    }
+      })
+      .setOn('fatalError', async (error) => {
+        console.error(error);
+      })
+      .build();
   })
-  .setOn('fatalError', async (error) => {
-    console.error(error);
-  })
-  .build()
   .then((agent) => {
     djanSeriy = agent;
     console.info('SETTING SHUTDOWN HOOK');
     process.on('SIGINT', agent.shutdown);
-    return agent;
-  })
-  .then((agent) => {
     console.info('RUNNING AGENT');
     return agent.run();
   });
