9c29489418e1012a15b658d2c56828808393061a
Ethan Fortner <ethfort@amazon.com>
2025-01-18 16:28:30 -0600

    fix(web): fixed exports and argument declaration

--- packages/web/src/actions/index.ts
+++ packages/web/src/actions/index.ts
@@ -7,3 +7,7 @@ export { isTavilySearchResult } from './tavily-search-action.guard';
 export { HttpGetAction } from './http-get-action';
 export { MarketQuote, MarketQuoteAction } from './market-quote-action';
 export { isMarketQuote } from './market-quote-action.guard';
+export {
+  HttpGetMarkdownAction,
+  HttpGetMarkdownActionProps,
+} from './http-get-markdown-action';
