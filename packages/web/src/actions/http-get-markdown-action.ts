0866973cafb0b3165661078d42502dbfd2bf1b38
Ethan Fortner <ethfort@amazon.com>
2025-01-18 16:28:30 -0600

    fix(web): fixed exports and argument declaration

--- packages/web/src/actions/http-get-markdown-action.ts
+++ packages/web/src/actions/http-get-markdown-action.ts
@@ -1,12 +1,25 @@
 import { HttpGetAction } from './http-get-action';
-import { Action, ActionArgument } from '@anaplian/core';
+import { Action } from '@anaplian/core';
 import TurndownService from 'turndown';
 
+/**
+ * Properties for HttpGetMarkdownAction.
+ */
 export interface HttpGetMarkdownActionProps {
+  /**
+   * A TurndownService for customizing the markdown conversion.
+   */
   readonly turndownService?: TurndownService;
+  /**
+   * An HttpGetAction for customizing the HTTP request.
+   */
   readonly parentAction?: HttpGetAction;
 }
 
+/**
+ * Performs an HTTP GET request and returns the raw content.
+ * HTML will be converted to markdown if possible.
+ */
 export class HttpGetMarkdownAction implements Action<'url'> {
   private readonly props: Required<HttpGetMarkdownActionProps>;
 
@@ -19,7 +32,17 @@ export class HttpGetMarkdownAction implements Action<'url'> {
   }
 
   public readonly name = 'httpGet';
-  arguments?: [ActionArgument<'url'>] | undefined;
+  public readonly arguments: Action<'url'>['arguments'] = [
+    {
+      name: 'url',
+      description: 'The address to get including protocol.',
+      exampleValidValues: [
+        'https://www.google.com',
+        'https://anaplian.example.com/a.json',
+      ],
+      exampleInvalidValues: ['google.com'],
+    },
+  ];
   public readonly description =
     'Performs an HTTP GET request to the desired URL. Use this action when you want to fetch the raw content from web site. ' +
     'If the site is HTML, the result will be converted into markdown format.';
