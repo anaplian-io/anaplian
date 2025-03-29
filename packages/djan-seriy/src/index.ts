b7b393caeac43ae9a2b26b790a6e401b0072d147
Ethan Fortner <30123404+efortner@users.noreply.github.com>
2025-03-29 16:23:15 -0500

    feat(djan-seriy): cleaned up and focussed the example agent (#133)

--- packages/djan-seriy/src/index.ts
+++ packages/djan-seriy/src/index.ts
@@ -9,109 +9,70 @@ import {
   StaticImageContextProvider,
   ThinkAction,
 } from '@anaplian/core';
-import { ChatOpenAI } from '@langchain/openai';
 import { getEnvironmentVariable } from './environment';
 import { HttpGetMarkdownAction, TavilySearchAction } from '@anaplian/web';
 import { tavily } from '@tavily/core';
-import { Client, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
-import {
-  GuildContextProvider,
-  ListChannelsAction,
-  MessageChannelAction,
-} from '@anaplian/discord';
+import { Ollama } from '@langchain/ollama';
 
-const openAiApiKey = getEnvironmentVariable('OPEN_AI_API_KEY');
 const directive = getEnvironmentVariable('DIRECTIVE');
 const tavilyApiKey = getEnvironmentVariable('TAVILY_API_KEY');
-const discordToken = getEnvironmentVariable('DISCORD_BOT_TOKEN');
 const imageUrl = getEnvironmentVariable('IMAGE_URL');
 let djanSeriy: AnaplianAgent | undefined;
-const discordClient = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-});
-discordClient
-  .login(discordToken)
-  .then(async () => {
-    console.info('CONNECTED TO DISCORD');
-    const modelName = 'gpt-4o-mini';
-    return new AgentBuilder({
-      modelName,
-      roleAssignmentDirective: directive,
-      model: new ChatOpenAI({
-        apiKey: openAiApiKey,
-        model: modelName,
-        streaming: false,
-        temperature: 0,
+(async () => {
+  const agent = await new AgentBuilder({
+    roleAssignmentDirective: directive,
+    model: new Ollama({
+      model: 'gemma3:27b',
+    }),
+  })
+    .setContextWindowSize(128_000)
+    .addAction(new NopAction())
+    .addAction(new ThinkAction())
+    .addAction(
+      new TavilySearchAction({
+        tavilyClient: tavily({ apiKey: tavilyApiKey }),
+        maxResults: 5,
       }),
+    )
+    .addAction(new HttpGetMarkdownAction({}))
+    .addContextProvider(new HistoryContextProvider({}), 97)
+    .addContextProvider(new DateContextProvider(), 0.5)
+    .addContextProvider(
+      new StaticImageContextProvider({
+        images: [
+          {
+            annotation: 'Input image',
+            imageType: 'jpeg',
+            imageContent: await fetch(imageUrl).then(async (response) =>
+              Buffer.from(await response.arrayBuffer()),
+            ),
+          },
+        ],
+      }),
+      0.5,
+    )
+    .setOn('beforeShutdown', async (context) => {
+      console.info('SHUTTING DOWN...');
+      console.info(serializeWithoutImages(context));
     })
-      .addAction(new NopAction())
-      .addAction(new ThinkAction())
-      .addAction(
-        new TavilySearchAction({
-          tavilyClient: tavily({ apiKey: tavilyApiKey }),
-          maxResults: 5,
-        }),
-      )
-      .addAction(
-        new ListChannelsAction({
-          discordClient,
-        }),
-      )
-      .addAction(
-        new MessageChannelAction({
-          discordClient,
-        }),
-      )
-      .addAction(new HttpGetMarkdownAction({}))
-      .addContextProvider(new HistoryContextProvider({}), 97)
-      .addContextProvider(new DateContextProvider(), 0.5)
-      .addContextProvider(
-        new GuildContextProvider({
-          discordClient,
-        }),
-        2,
-      )
-      .addContextProvider(
-        new StaticImageContextProvider({
-          images: [
-            {
-              annotation: 'Input image',
-              imageType: 'jpeg',
-              imageContent: await fetch(imageUrl).then(async (response) =>
-                Buffer.from(await response.arrayBuffer()),
-              ),
-            },
-          ],
-        }),
-        0.5,
-      )
-      .setOn('beforeShutdown', async (context) => {
-        console.info('SHUTTING DOWN...');
-        console.info(serializeWithoutImages(context));
-      })
-      .setOn('afterIterationEnd', async (context) => {
-        const history = context.history;
-        if (isHistoryContext(history)) {
-          const lastItem = history.records[history.records.length - 1];
-          if (lastItem) {
-            console.info(`AGENT: ${lastItem.actionTaken}`);
-            console.info(`RESULT: ${lastItem.result}`);
-            if (lastItem.actionTaken === 'nop()') {
-              await djanSeriy?.shutdown().then(() => process.exit(0));
-            }
+    .setOn('afterIterationEnd', async (context) => {
+      const history = context.history;
+      if (isHistoryContext(history)) {
+        const lastItem = history.records[history.records.length - 1];
+        if (lastItem) {
+          console.info(`AGENT: ${lastItem.actionTaken}`);
+          console.info(`RESULT: ${lastItem.result}`);
+          if (lastItem.actionTaken === 'nop()') {
+            await djanSeriy?.shutdown().then(() => process.exit(0));
           }
         }
-      })
-      .setOn('fatalError', async (error) => {
-        console.error(error);
-      })
-      .build();
-  })
-  .then((agent) => {
-    djanSeriy = agent;
-    console.info('SETTING SHUTDOWN HOOK');
-    process.on('SIGINT', agent.shutdown);
-    console.info('RUNNING AGENT');
-    return agent.run();
-  });
+      }
+    })
+    .setOn('fatalError', async (error) => {
+      console.error(error);
+    })
+    .build();
+  process.on('SIGINT', agent.shutdown);
+  await agent.run();
+})();
