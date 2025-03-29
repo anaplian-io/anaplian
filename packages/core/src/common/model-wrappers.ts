74cdadf2fe719e154316d1de5357c1bd44a867ea
Ethan Fortner <30123404+efortner@users.noreply.github.com>
2025-03-29 17:13:55 -0500

    fix(core): fixed image parsing, which only works for chat models (#134)

--- packages/core/src/common/model-wrappers.ts
+++ packages/core/src/common/model-wrappers.ts
@@ -2,50 +2,47 @@ import { AnaplianModel, isDefined } from './types';
 import { RootFormatter } from '../formatters/root-formatter';
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { BaseLanguageModel } from '@langchain/core/language_models/base';
-import { MessageContentComplex, SystemMessage } from '@langchain/core/messages';
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
 
 export const wrapModel = (
   model: BaseLanguageModel,
   rootFormatter: RootFormatter,
 ): AnaplianModel => ({
   invoke: async (context) => {
-    const contextImages: MessageContentComplex[] = Object.values(context)
+    const contextImages: HumanMessage[] = Object.values(context)
       .map((context) => context.IMAGES)
       .filter(isDefined)
       .flatMap((imageCollection) =>
         imageCollection.map(
           (imageDefinition) =>
-            <MessageContentComplex>[
-              {
-                type: 'text',
-                text: imageDefinition.annotation,
-              },
-              {
-                type: 'image_url',
-                image_url: {
-                  detail: imageDefinition.imageDetail,
-                  url: `data:image/${imageDefinition.imageType};base64,${imageDefinition.imageContent.toString('base64')}`,
+            new HumanMessage({
+              content: [
+                {
+                  type: 'text',
+                  text: imageDefinition.annotation,
                 },
-              },
-            ],
+                {
+                  type: 'image_url',
+                  image_url: {
+                    detail: imageDefinition.imageDetail,
+                    url: `data:image/${imageDefinition.imageType};base64,${imageDefinition.imageContent.toString('base64')}`,
+                  },
+                },
+              ],
+            }),
         ),
       );
-    const messages: MessageContentComplex[] = [
-      ...contextImages,
-      {
-        type: 'text',
-        text: await rootFormatter.formatPartial(),
-      },
-      {
-        type: 'text',
-        text: rootFormatter.serializeContext(context),
-      },
-    ];
-    return model.pipe(new StringOutputParser()).invoke([
-      new SystemMessage({
-        content: messages,
-      }),
-    ]);
+    const systemMessage = new SystemMessage({
+      content: [
+        {
+          type: 'text',
+          text: `${await rootFormatter.formatPartial()}\n\n${rootFormatter.serializeContext(context)}`,
+        },
+      ],
+    });
+    return model
+      .pipe(new StringOutputParser())
+      .invoke([systemMessage, ...contextImages]);
   },
   getTokenCount: (content) => model.getNumTokens(content),
 });
